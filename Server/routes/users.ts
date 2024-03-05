90c13424d5e03bc52c0a17783420a44d3227b56c
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-05 23:56:32 +0530

    ✨ Added the Event Emitter function

--- Server/routes/users.ts
+++ Server/routes/users.ts
@@ -1,12 +1,28 @@
 import {t} from "../trpc"
 import {z} from "zod";
+import {EventEmitter} from "events";
+import { observable } from "@trpc/server/dist/observable";
 
+
+const ee = new EventEmitter();
 const procedures = [
     t.procedure.input(z.object({userId: z.string()})),
 ]
 
 export const userRouter = t.router({
     getUser: procedures[0].query((req)=>{
+        ee.emit("getUser", req.input.userId)
         return req.input.userId;
+    }),
+    updateUser: t.procedure.subscription(()=>{
+        return observable<string>((emit)=>{
+            const onGetUser = (userId:string)=>{
+                emit.next(userId);
+            }
+            ee.on("getUser", onGetUser);
+            return ()=>{
+                ee.off("getUser", onGetUser);
+            }
+        })
     })
 })
